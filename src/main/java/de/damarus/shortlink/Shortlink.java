e6afb61a0d4890c3d887870e2d80097b4576ee94
Dam4rusxp <dam4rusxp@googlemail.com>
2017-10-09 02:53:37 +0200

    Fix high cpu usage due to Thread.sleep() not being reached

--- src/main/java/de/damarus/shortlink/Shortlink.java
+++ src/main/java/de/damarus/shortlink/Shortlink.java
@@ -9,6 +9,7 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 
 public class Shortlink extends JFrame {
 
@@ -24,34 +25,55 @@ public class Shortlink extends JFrame {
             System.exit(-1);
         }
 
-        // Continuously check clipboard for changes, ignore known unproblematic errors
         while (true) {
             try {
-                String clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
-                if (!clipboard.equals(lastClipboard)) {
+                String clipboard = getStringClipboard();
+                if (clipboard != null && !clipboard.equals(lastClipboard)) {
+                    lastClipboard = clipboard;
+
                     if (isLink(clipboard)) {
                         System.out.println("Handling link: " + clipboard);
-                        ModifieableLink link = ModifieableLink.fromURL(new URL(clipboard));
-
-                        // Don't be annoying when the link can't be changed anyways
-                        if (link.isActuallyModifieable()) {
-                            String beforeRules = link.toString();
-                            RuleManager.applyRulesTo(link);
-
-                            // Display the window if we can modify parameters, or the path changed
-                            if (!link.getParameters().isEmpty() || !beforeRules.equals(link.toString())) {
-                                getWindow().displayLink(link);
-                            }
-                        }
+                        handleNewLink(clipboard);
                     }
-
-                    lastClipboard = clipboard;
                 }
-
-                Thread.sleep(1000 / 5);
-            } catch (UnsupportedFlavorException | IllegalStateException ignored) {
-            } catch (IOException | InterruptedException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                try {
+                    Thread.sleep(1000 / 5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private static String getStringClipboard() {
+        DataFlavor[] currentFlavors = Toolkit.getDefaultToolkit().getSystemClipboard().getAvailableDataFlavors();
+        if (Arrays.stream(currentFlavors).anyMatch(DataFlavor::isFlavorTextType)) {
+            try {
+                return (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+            } catch (UnsupportedFlavorException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    private static void handleNewLink(String clipboard) throws MalformedURLException {
+        ModifieableLink link = ModifieableLink.fromURL(new URL(clipboard));
+
+        // Don't be annoying when the link can't be changed anyways
+        if (link.isActuallyModifieable()) {
+            String beforeRules = link.toString();
+            RuleManager.applyRulesTo(link);
+
+            // Display the window if we can modify parameters, or the path changed
+            // That means we don't find fragments annoying enough to show the window
+            if (!link.getParameters().isEmpty() || !beforeRules.equals(link.toString())) {
+                getWindow().displayLink(link);
             }
         }
     }
